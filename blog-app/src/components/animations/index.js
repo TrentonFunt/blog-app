54cf964284b4882872416b1148307ed6b243118c
TrentonFunt <ddejilade22@gmail.com>
2025-09-16 15:19:58 +0100

    Remove nested blog-app folder to clean up repository structure
    - Removed duplicate nested blog-app/ directory
    - Kept all files in root directory for cleaner structure
    - Repository now has proper flat structure for deployment

--- blog-app/src/components/animations/index.js
+++
@@ -1,8 +0,0 @@
-// Animation Components Export
-export { default as FadeInUp } from './FadeInUp';
-export { default as StaggerContainer, StaggerItem } from './StaggerContainer';
-export { default as HoverScale } from './HoverScale';
-export { default as PageTransition } from './PageTransition';
-export { default as AnimatedButton } from './AnimatedButton';
-export { default as ParallaxScroll } from './ParallaxScroll';
-export { default as TextReveal } from './TextReveal';
